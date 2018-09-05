3d37158e82a5d03f8152acbb4ad61ebaceb71a39
Arseny Tolmachev <eiennohito@gmail.com>
2018-09-05 13:55:20 +0900

    data preparation for XMorph

--- example/src/main/scala/ws/kotonoha/spark/tensorflow/TFExample.scala
+++ example/src/main/scala/ws/kotonoha/spark/tensorflow/TFExample.scala
@@ -3,8 +3,7 @@ package ws.kotonoha.spark.tensorflow
 import com.google.protobuf.ByteString
 import org.tensorflow.example.example.Example
 import org.tensorflow.example.feature.Feature.Kind
-import org.tensorflow.example.feature.{BytesList, Feature, Features, FloatList, Int64List}
-
+import org.tensorflow.example.feature._
 
 /**
   * @author eiennohito
@@ -13,8 +12,8 @@ import org.tensorflow.example.feature.{BytesList, Feature, Features, FloatList, 
 object TFExample {
   def apply(data: (String, Feature)*): Example = Example(Some(Features(data.toMap)))
   def i64(data: Long*): Feature = i64seq(data)
-  def f32(data: Float*): Feature = Feature(Kind.FloatList(FloatList(data)))
-  def i64seq(data: Seq[Long]): Feature = Feature(Kind.Int64List(Int64List(data)))
+  def f32(data: Float*): Feature = Feature(Kind.FloatList(FloatList(data.toArray)))
+  def i64seq(data: Seq[Long]): Feature = Feature(Kind.Int64List(Int64List(data.toArray)))
   def byteSeq(data: Seq[ByteString]): Feature = {
     Feature(Kind.BytesList(
       BytesList(data)
